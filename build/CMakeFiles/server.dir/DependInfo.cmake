3776e0e30b0d440b58a7e11bab66d42a68a89c9e
kbjcx <lu.kbjcx@gmail.com>
2023-03-10 09:41:40 +0800

    修改gitignore

--- build/CMakeFiles/server.dir/DependInfo.cmake
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/llz/CPP/RTSP_Server/main.cpp" "/home/llz/CPP/RTSP_Server/build/CMakeFiles/server.dir/main.cpp.o"
+  "/home/llz/CPP/tmp/tmp.qETx2QyDBq/main.cpp" "/home/llz/CPP/tmp/tmp.qETx2QyDBq/build/CMakeFiles/server.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
